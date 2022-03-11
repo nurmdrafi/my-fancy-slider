6b9c1a792e2614c1aee7bfbd37feb48551c3a92a
nurmdrafi <nurmdrafi2014@gmail.com>
2022-03-11 21:29:38 +0600

    initial commit

---
+++ slider.js
@@ -0,0 +1,12 @@
+const images = ['img/pic-1.jpg',
+'img/pic-2.jpg',
+'img/pic-3.jpg',
+'img/pic-4.jpg',
+'img/pic-5.jpg',
+'img/pic-6.jpg',
+'img/pic-7.jpg',
+]
+setInterval(() => {
+
+    console.log(new Date().getTime())
+}, 1000);
